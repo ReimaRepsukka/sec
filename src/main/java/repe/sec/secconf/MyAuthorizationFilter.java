95d52f50af2221d15d671217cdd182c46a129106
repsukka <81526112+repsukka@users.noreply.github.com>
2021-11-14 23:34:38 +0200

    changes

--- src/main/java/repe/sec/secconf/MyAuthorizationFilter.java
+++ src/main/java/repe/sec/secconf/MyAuthorizationFilter.java
@@ -7,7 +7,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties.Jwt;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
--- src/main/java/repe/sec/secconf/MySecurityConfiruration.java
+++ src/main/java/repe/sec/secconf/MySecurityConfiruration.java
@@ -2,7 +2,6 @@ package repe.sec.secconf;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
